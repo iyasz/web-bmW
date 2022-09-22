2733a5e4aa532c59a75841a20b2f1f560670f192
Yasz Avellia <zakamaragames@gmail.com>
2022-09-22 20:34:03 +0700

    menambah db

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
+//helper
 include "app/function.php";
 
+// database
+include "app/db.php";
+
+// header
 include "layouts/header.php";
 // include_once "layouts/header.php";
 
+//views
 include "app/view.php";
 
+// footer
 include "layouts/footer.php";
--- views/auth/login.php
+++ views/auth/login.php
@@ -1,13 +1,30 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-6">
-            <div class="card shadow">
-                <div class="card-header text-center">
-                    <h5>Daring BM3 · Online Learning</h5>
-                    <P>Learning starts at 08:00 - 17:00 AM</P>
-                </div>
-                <div class="card-body">
-
+<div class="bc-200">
+    <div class="container position-relative  pr">
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <div class="card shadow">
+                    <div class="card-header text-center py-4 position-relative  bc-100">
+                        <h5>Daring BM3 · Online Learning</h5>
+                        <P>Learning starts at 08:00 - 17:00 AM</P>
+                        <a class="bg-white pl " href=""><img width="55px" src="<?= base_url() ?>asset/img/logo/logobmw.gif" alt=""></a>
+                    </div>
+                    <div class="card-body ">
+                        <form action="" method="post">
+                            <div class="mb-3 mt-5 hp">
+                                <label for="user">Username</label>
+                                <input type="text" name="username" id="user" autocomplete="off" class="form-control">
+                                <p></p>
+                            </div>
+                            <div class="mb-3 hp">
+                                <label for="pw">Password</label>
+                                <input type="text" name="password" id="pw" class="form-control" autocomplete="off">
+                                <p></p>
+                            </div>
+                            <div class="footer text-center mt-5 mb-4">
+                                <button class="btn btn-primary">login</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
