63a6c8892da2e4dcf9231929d176af5e210384c2
Yasz Avellia <zakamaragames@gmail.com>
2022-09-22 19:29:52 +0700

    menambah img logo

--- layouts/header.php
+++ layouts/header.php
@@ -5,7 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
+    <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <!-- costumCSS -->
+    <link rel="stylesheet" href="<?= base_url() ?>asset/css/custom.css">
 </head>
 
 <body>
